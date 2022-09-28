b8569541fa382f0fee008af025350d5550a04b68
user <jongwoop97@gmail.com>
2022-09-28 23:27:21 +0900

    array stack boilerplate

--- Implementations/stack_arr.js
+++ Implementations/stack_arr.js
@@ -1,72 +1,58 @@
-class Stack {
-    constructor() {
-        this.top = null;
-        this.bottom = null;
-        this.length = 0;
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
     }
-
+  }
+  
+  class Stack {
+    constructor(){
+      this.top = null;
+      this.bottom = null;
+      this.length = 0;
+    }
     peek() {
-        return this.top;
+      return this.top;
     }
-
-    push(value) {
-        const newNode = new Node(value);
-        // could also check length for emtpy
-        if (this.bottom === null) {
-            this.top = newNode;
-            this.bottom = newNode;
-        } else {
-            this.top.next = newNode;
-            this.top = newNode;
-        }
-        this.length++;
+    push(value){
+      const newNode = new Node(value);
+      if (this.length === 0) {
+        this.top = newNode;
+        this.bottom = newNode;
+      } else {
+        const holdingPointer = this.top;
+        this.top = newNode;
+        this.top.next = holdingPointer;
+      }
+      this.length++;
+      return this;
     }
-
-    pop() {
-        if (this.isEmpty()){
-            return null;
-        }else if(this.top === this.bottom){
-            const final = this.top;
-            this.top = null;
-            this.bottom = null;
-            this.length--;
-            return final
-        } else {
-            let prev_top = this.top;
-            let new_top = this.bottom;
-            while(new_top.next.next != null){
-                new_top = new_top.next;
-            }
-            this.top = new_top;
-            this.top.next = null;
-            this.length--;
-            return prev_top;
-        }
+    pop(){
+      if (!this.top) {
+        return null;
+      }
+      if (this.top === this.bottom) {
+        this.bottom = null;
+      }
+      const holdingPointer = this.top;
+      this.top = this.top.next;
+      this.length--;
+      return this;
     }
-
-    isEmpty() {
-        return !(this.length);
-    }
-}
-
-const myStack = new Stack();
-// myStack.push("Google")
-// console.log(myStack.peek())
-// myStack.push("Udemy")
-// console.log(myStack.peek())
-// myStack.push("Discord")
-// console.log(myStack.peek())
-
-// console.log(myStack.pop().value)
-// console.log(myStack.pop().value)
-// console.log(myStack.pop().value)
-// console.log(myStack.isEmpty())
-
-myStack.peek();
-myStack.push('google');
-myStack.push('udemy');
-myStack.push('discord');
-console.log(myStack.peek());
-console.log(myStack.pop());
-console.log(myStack.pop());
-console.log(myStack.pop());
+    //isEmpty
+  }
+  
+  const myStack = new Stack();
+  myStack.peek();
+  myStack.push('google');
+  myStack.push('udemy');
+  myStack.push('discord');
+  myStack.peek();
+  myStack.pop();
+  myStack.pop();
+  myStack.pop();
+  
+  
+  //Discord
+  //Udemy
+  //google
