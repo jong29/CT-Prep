1d410a31885eea5c2d976f48b9979e9986a3ba12
user <jongwoop97@gmail.com>
2022-12-16 22:57:06 +0900

    my implementation of bst

--- Implementations/bst.js
+++ Implementations/bst.js
@@ -6,36 +6,73 @@ class Node {
     }
   }
   
-  class BinarySearchTree {
-    constructor(){
-      this.root = null;
+class BinarySearchTree {
+constructor(){
+    this.root = null;
+}
+insert(value){
+    let nn = new Node(value);
+    let it = this.root;
+    //Code here
+    if (it === null) {
+        this.root = nn;
+        return;
+    } 
+    
+    while(true){
+        if (it.value < nn.value) {
+            if(it.right === null){
+                it.right = nn;
+                break;
+            } else {
+                it = it.right;
+            }
+        } else {
+            if(it.left === null){
+                it.left = nn;
+                break;
+            } else {
+                it = it.left;
+            }
+        }
     }
-    insert(value){
-      //Code here
+}
+lookup(value){
+    //Code here
+    const it = this.root;
+    while(true) {
+        if (it === null) {
+            return null;
+        } else if (it.value === value) {
+            return it;
+        } else if (it.value < value) {
+            it = it.right;
+        } else if (it.value >= value) {
+            it = it.left;
+        }
     }
-    lookup(value){
-      //Code here
-    }
-    // remove
-  }
-  
-  const tree = new BinarySearchTree();
-  tree.insert(9)
-  tree.insert(4)
-  tree.insert(6)
-  tree.insert(20)
-  tree.insert(170)
-  tree.insert(15)
-  tree.insert(1)
-  JSON.stringify(traverse(tree.root))
-  
-  //     9
-  //  4     20
-  //1  6  15  170
-  
-  function traverse(node) {
-    const tree = { value: node.value };
-    tree.left = node.left === null ? null : traverse(node.left);
-    tree.right = node.right === null ? null : traverse(node.right);
-    return tree;
-  }
+}
+// remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9)
+console.log(tree)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+JSON.stringify(traverse(tree.root))
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+const tree = { value: node.value };
+tree.left = node.left === null ? null : traverse(node.left);
+tree.right = node.right === null ? null : traverse(node.right);
+return tree;
+}
