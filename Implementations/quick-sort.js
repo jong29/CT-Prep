be04f4e438df30987279ea5b8eebd67a8aa6a85b
user <jongwoop97@gmail.com>
2023-01-10 21:16:58 +0900

    quicksort2

--- Implementations/quick-sort.js
+++ Implementations/quick-sort.js
@@ -1,7 +1,23 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function quickSort(array, left, right){
-    
+    let pivot = Math.floor((left+right)/2);
+    let pval = array[pivot];
+    let tmp = array[left];
+    array[left] = array[pivot];
+    array[pivot] = tmp;
+    left++;
+
+    while(left <= right) {
+        if(array[left] <= pval) {
+            left++;
+        }
+        if(array[right] > pval) {
+            right--;
+        }
+    }
+
+    return quickSort(array, left, right);
 }
 
 
