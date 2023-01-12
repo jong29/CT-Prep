66e7e6b96e5e362401c4092faf6343360dbe30fb
user <jongwoop97@gmail.com>
2023-01-12 23:25:59 +0900

    quicksort solution

--- Implementations/quick-sort.js
+++ Implementations/quick-sort.js
@@ -1,25 +1,40 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function quickSort(array, left, right){
-    let pivot = Math.floor((left+right)/2);
-    let pval = array[pivot];
-    let tmp = array[left];
-    array[left] = array[pivot];
-    array[pivot] = tmp;
-    left++;
+  const len = array.length; 
+  let pivot;
+  let partitionIndex;
 
-    while(left <= right) {
-        if(array[left] <= pval) {
-            left++;
-        }
-        if(array[right] > pval) {
-            right--;
-        }
+  if(left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+    
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+   
+function partition(array, pivot, left, right){
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for(let i = left; i < right; i++) {
+    if(array[i] < pivotValue){
+      swap(array, i, partitionIndex);
+      partitionIndex++;
     }
-
-    return quickSort(array, left, right);
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
 }
 
+function swap(array, firstIndex, secondIndex){
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
 
 //Select first and last index as 2nd and 3rd parameters
 quickSort(numbers, 0, numbers.length - 1);
